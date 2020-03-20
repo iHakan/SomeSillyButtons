30d1cd36030d905ff8856b3798372345198c7695
iHakan <hakan@re-coded.com>
2020-03-21 00:26:30 +0300

    Codes added for HTML and JS files

---
+++ index.js
@@ -0,0 +1,15 @@
+function tiklamaca(){
+    document.getElementById("tıkla-gelsin").innerText = "Evet başardın ilk butonun JS ile çalışıyor!"
+}
+
+function hover(){
+    document.getElementById("hover").innerHTML ="Ahada mouse buton üzerinde!!!";
+}
+
+function unHover(){
+    document.getElementById("hover").innerHTML="Mouse buton üzerinde değil!!!";
+}
+
+function tusabas(){
+    document.getElementById("tus").innerHTML="İşte bu! Artık bir buton master ısın!!! Tebrikler Hakan <3 ^_^ ";
+}
